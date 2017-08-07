d7c0f838d898c23d43af0b0ed6ffaaaacbdf5306
surajshetiya <surajshetiya@gmail.com>
2017-08-07 10:44:21 -0500

    Added facebook challenges

---
+++ Facebook/Reverse Polish Notation/Answer.py
@@ -0,0 +1,70 @@
+testcases = int(raw_input())
+lookup = [[]]
+text = ''
+
+def lookup_value(x, y, length):
+    global lookup
+    if(len(lookup[0]) != 0):
+        return lookup[x][y]
+    else:
+        lookup = []
+        for times in range(0,length):
+            lookup.append([-1]*length)
+        return lookup[x][y]
+
+def solve(start, end):
+    global text
+    global lookup
+    length = len(text)
+    if(lookup_value(start, end, length) == -1):
+        minimum = float('Inf')
+
+        if(start == end):
+            if(text[start] == 'x'):
+                lookup[start][start] = 0
+            else:
+                lookup[start][start] = 1
+            #print("Lookup " + str(start) + " to " + str(end) + " " + str(lookup[start][start]))
+            return lookup[start][start]
+
+        # Add a character * to the end and run combinations of 0 to x and x+1 to n
+        for mid in range(start, end):
+            value = solve( start, mid ) + solve( mid+1, end ) + 1
+            if( value < minimum ):
+                #print("Lookup add a * split at " + str(mid) + " for "  + str(start) + " to " + str(end) + " " + str(value))
+                minimum = value
+
+        # Change a character at right end  to * if it is x
+        if(text[end] == '*'):
+            basic = 0
+        else:
+            basic = 1
+        for mid in range(start, end):
+            if(mid <= end - 2 ):
+                value = solve(start, mid) + solve(mid + 1, end - 1)
+            else:
+                value = solve(start, mid - 1) + 1 # Add an 'x' char
+            if(value + basic < minimum):
+                #print("Lookup change the last char split at " + str(mid) + " for " + str(start) + " to " + str(end) + " with basic as " + str(basic) + " "+ str(value + basic))
+                minimum = value + basic
+
+        # Check combinations of deletions and size from right and left
+        for e in range(start, end):
+            value = end - e + solve(start, e)
+            if( value < minimum ):
+                minimum = value
+            value = e + 1 - start + solve(e + 1, end)
+            if (value < minimum):
+                minimum = value
+
+        lookup[start][end] = minimum
+        return minimum
+    else:
+        return lookup[start][end]
+
+for t in range(0, testcases):
+    global lookup
+    global text
+    lookup = [[]]
+    text = raw_input()
+    print(solve(0, len(text)-1))
